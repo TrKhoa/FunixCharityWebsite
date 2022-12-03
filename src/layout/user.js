476b81d9f8210a0621c2097bfa27f9023a43fe0d
TrKhoa <khoatmtfx14250@funix.edu.vn>
2022-12-03 12:06:59 +0700

    404 page added

--- src/layout/user.js
+++ src/layout/user.js
@@ -1,6 +1,7 @@
 import { Routes,Route,Outlet } from 'react-router-dom';
 import Home from '../pages/user/home';
 import Menu from '../components/user/Navbar'
+import Err404 from './404';
 import Footer from '../components/user/Footer'
 
 export default function User() {
@@ -19,7 +20,7 @@ export default function User() {
                 <Route element={<BasicLayout />} >
                     <Route path="/">
                         <Route index element={<Home />} />
-                        <Route path="*" element={<Home />} />
+                        <Route path="*" element={<Err404 />} />
                     </Route>
                 </Route>
             </Routes>
