b06dbde1af76ec440d9e03564be6890aedd28375
TrKhoa <khoatmtfx14250@funix.edu.vn>
2022-12-30 00:28:06 +0700

    pagination added

--- src/components/causes/Causes.js
+++ src/components/causes/Causes.js
@@ -1,5 +1,5 @@
+import { Link, useLocation } from "react-router-dom";
 import Campaign from "../campaigns/Campaign";
-
 export default function Causes(args) {
     /*
     const CampaignGroup = () => {
@@ -18,37 +18,108 @@ export default function Causes(args) {
             ));
     };
     */
+
+    const search = useLocation().search;
+    const query = new URLSearchParams(search);
+    const itemsPerPage = 6;
     const campaigns = args.campaigns;
-    const {title} = args || '';
-    const extraInfo = () =>{
-        if(title === '' || title === undefined){
-            return {display: 'd-none', limit: 12};
-        }
-        else
-            return {display: '', limit: 6};
+    const total = campaigns.data.length;
+    const lastPage = Math.ceil(total / itemsPerPage);
+    let page = parseInt(query.get("page")) || 1;
+    if(page < 0 || page > lastPage){
+        page = 1;
     }
+    const nextPage = itemsPerPage * page < total;
+    const { title } = args || "";
+    const extraInfo = () => {
+        if (title === "" || title === undefined) {
+            return {
+                display: "d-none",
+                limit: page * itemsPerPage,
+                skip: (page - 1) * itemsPerPage,
+            };
+        } else return { display: "", limit: 6 };
+    };
+
+    const Pagination = () => {
+        return (
+            <ul class="pagination justify-content-center pt-5">
+                {page > 1 ? (
+                    <li class="page-item">
+                        <Link to="/Cause?page=1" class="page-link">
+                            Trang đầu
+                        </Link>
+                    </li>
+                ) : (
+                    <li class="page-item">
+                        <span class="page-link text-secondary">Trang đầu</span>
+                    </li>
+                )}
+                {page > 1 ? (
+                    <li class="page-item">
+                        <Link
+                            to={"/Cause?page=" + (page - 1)}
+                            class="page-link"
+                        >
+                            {page - 1}
+                        </Link>
+                    </li>
+                ) : (
+                    ""
+                )}
+                <li class="page-item active" aria-current="page">
+                    <span class="page-link">{page}</span>
+                </li>
+                {nextPage ? (
+                    <>
+                        <li class="page-item" aria-current="page">
+                            <Link
+                                to={"/Cause?page=" + (page + 1)}
+                                class="page-link"
+                            >
+                                {page + 1}
+                            </Link>
+                        </li>
+                        <li class="page-item">
+                            <Link to={"/Cause?page=" + lastPage} class="page-link">
+                                Trang cuối
+                            </Link>
+                        </li>
+                    </>
+                ) : (
+                    <li class="page-item">
+                        <span class="page-link text-secondary">
+                            Trang cuối
+                        </span>
+                    </li>
+                )}
+            </ul>
+        );
+    };
     return (
         <div className="container col-xxl-12">
             <div className={extraInfo().display}>
-            <div className="row align-items-center justify-content-center g-5 py-5">
-                <h1 className="display-5 fw-bold lh-3 mb-3 col-xxl-8 text-center">
-                    {title}
-                </h1>
-                <p className="lead">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                    occaecat cupidatat non proident, sunt in culpa qui officia
-                    deserunt mollit anim id est laborum.
-                </p>
-            </div>
+                <div className="row align-items-center justify-content-center g-5 py-5">
+                    <h1 className="display-5 fw-bold lh-3 mb-3 col-xxl-8 text-center">
+                        {title}
+                    </h1>
+                    <p className="lead">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco laboris nisi ut aliquip ex ea
+                        commodo consequat. Duis aute irure dolor in
+                        reprehenderit in voluptate velit esse cillum dolore eu
+                        fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                        non proident, sunt in culpa qui officia deserunt mollit
+                        anim id est laborum.
+                    </p>
+                </div>
             </div>
             <div className="row align-items-center justify-content-center">
-                {
-                campaigns.data.slice(0, extraInfo().limit).map((val) => (
+                {campaigns.data
+                    .slice(extraInfo().skip, extraInfo().limit)
+                    .map((val) => (
                         <Campaign
                             id={val._id}
                             title={val.name}
@@ -58,9 +129,9 @@ export default function Causes(args) {
                             goal={val.goal}
                             desc={val.desc}
                         />
-                    ))
-                }
+                    ))}
             </div>
+            <Pagination />
         </div>
     );
 }
